f44e13fd35495fe15cc32cadacef8d682801e1fa
Hernan Rodriguez Colmeiro <colmeiro@gmail.com>
2015-07-13 01:05:30 -0300

    Cosmetic changes, added uncollectible filter.

--- js/main.js
+++ js/main.js
@@ -10,32 +10,34 @@ if (!localStorage.getItem('cards')){
 }
 
 function setUpTable(){
-    var cards = JSON.parse(localStorage.getItem('cards'));
+    window.cards = JSON.parse(localStorage.getItem('cards'));
     var table = $('tbody');
+    $('#hide-uncollectible').prop('checked', true);
+    $('.search').val('');
     $.each(cards, function(set_name, set){
         if (set_name != 'Debug' && set_name != "Promotion") {
             $.each(set, function(index, card){
-                if (card.hasOwnProperty('cost') && card.type != "Hero Power" && card.type != "Hero") {
-                    var row = $('<tr>');
-                    row.append($('<td class="cost">' + card.cost + '</td>'));
+                if (card.hasOwnProperty('cost') && card.hasOwnProperty('rarity') && card.type != "Hero Power" && card.type != "Hero") {
+                    var row = $('<tr class="' + (card.collectible ? 'collectible' : 'uncollectible hide') + '">');
+                    row.append($('<td class="cost text-center">' + card.cost + '</td>'));
                     row.append($('<td class="name">' + card.name + '</td>'));
 
                     if (card.hasOwnProperty('attack')) {
-                        row.append($('<td>' + card.attack + '</td>'));
+                        row.append($('<td class="text-center">' + card.attack + '</td>'));
                     } else {
                         row.append($('<td></td>'));
                     }
 
                     if (card.hasOwnProperty('health')) {
-                        row.append($('<td>' + card.health + '</td>'));
+                        row.append($('<td class="text-center">' + card.health + '</td>'));
                     } else if (card.hasOwnProperty('duration')) {
-                        row.append($('<td>' + card.durability + '</td>'));
+                        row.append($('<td class="text-center">' + card.durability + '</td>'));
                     } else {
                         row.append($('<td></td>'));
                     }
 
                     if (card.hasOwnProperty('text')) {
-                        row.append($('<td>' + card.text.replace(/\$([0-9]+)/, '*$1*' ) + '</td>'));
+                        row.append($('<td class="text">' + card.text.replace(/\$([0-9]+)/, '*$1*' ) + '</td>'));
                     } else {
                         row.append($('<td></td>'));
                     }
@@ -46,7 +48,7 @@ function setUpTable(){
                         row.append($('<td class="race"></td>'));
                     }
 
-                    row.append($('<td>' + set_name + '</td>'));
+                    row.append($('<td class="set ' + card.rarity.toLowerCase() + '">' + set_name + '</td>'));
                     row.append($('<td><a target="_blank" href="' + CARDS_URL + card.id + '.png">Image</a></td>'))
                     table.append(row);
                 }
@@ -54,11 +56,22 @@ function setUpTable(){
         }
     });
 
+    // Don't submit form on button press
+    $('form').on('submit', function(e){
+        e.preventDefault();
+    });
+
     // Filters
     var options = {
-      valueNames: [ 'name', 'cost', 'race' ]
+      valueNames: [ 'name', 'cost', 'race', 'text']
     };
 
-    var cardList = new List('cards', options);
+    var cardList = new List('content', options);
 
+    $('#hide-uncollectible').on('change', function(e){
+        $('.uncollectible').removeClass('hide');
+        if (this.checked) {
+            $('.uncollectible').addClass('hide');
+        }
+    });
 }
